2b1fe192ce9b4b82df271da7850306e281178cb9
Haris Amin <aminharis7@gmail.com>
2022-11-17 00:25:21 -0500

    Parsing json log file from cli args and extracted import to function

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,57 @@
 use std::io::BufRead;
 use std::fs::File;
 use std::io::BufReader;
+use std::path::PathBuf;
+use structopt::StructOpt;
+use std::fs;
 
 
 use rusqlite::{Connection as SqliteConnection};
 
+#[derive(StructOpt, Debug)]
+#[structopt(name = "basic")]
+struct Opt {
+    // A flag, true if used in the command line. Note doc comment will
+    // be used for the help message of the flag. The name of the
+    // argument will be, by default, based on the name of the field.
+    /// Activate debug mode
+    // #[structopt(short, long)]
+    // debug: bool,
+
+    // The number of occurrences of the `v/verbose` flag
+    /// Verbose mode (-v, -vv, -vvv, etc.)
+    // #[structopt(short, long, parse(from_occurrences))]
+    // verbose: u8,
+
+    /// Set speed
+    // #[structopt(short, long, default_value = "42")]
+    // speed: f64,
+
+    // /// Output file
+    // #[structopt(short, long, parse(from_os_str))]
+    // output: PathBuf,
+
+    // the long option will be translated by default to kebab case,
+    // i.e. `--nb-cars`.
+    /// Number of cars
+    // #[structopt(short = "c", long)]
+    // nb_cars: Option<i32>,
+
+    /// admin_level to consider
+    #[structopt(short, long)]
+    query: Option<String>,
+
+    /// Files to process
+    #[structopt(name = "FILE", parse(from_os_str))]
+    files: Vec<PathBuf>,
+}
+
 fn main() {
-    let filename = "/Users/harisamin/foo.log";
+    let opt = Opt::from_args();
+    let _query = opt.query;
+    // println!("{:#?}", opt);
+    // println!("{:#?}", opt.query);
+
     let conn = SqliteConnection::open("test.db").unwrap();
     conn.execute_batch(&format!(r#"
         CREATE TABLE IF NOT EXISTS logs (id INTEGER PRIMARY KEY, filename TEXT, json_line JSON);
@@ -14,9 +59,18 @@ fn main() {
         "#)
     ).unwrap();
 
+    for f in opt.files {
+        import_logfile(&f, &conn);
+    }
 
-    let f = File::open(filename).unwrap();
+    conn.close().unwrap();
+}
 
+fn import_logfile(pb:&PathBuf, conn:&rusqlite::Connection) {
+    let full_path =  fs::canonicalize(pb).unwrap();
+    let filename = full_path.display();
+    // println!("Full filepath: {}", filename);
+    let f = File::open(&full_path).unwrap();
 
     let reader = BufReader::new(f);
     for line in reader.lines() {
@@ -38,19 +92,10 @@ fn main() {
                     }
                 }
             }
-            Err(_) => {
-                panic!("something went wrong")
+            Err(err) => {
+                panic!("Error reading lines: {:#}", err);
             }
         }
     }
-
-    conn.close().unwrap();
 }
 
-// fn filename() -> Option<()> {
-//     let file = "hey.text";
-//     let path = Path::new(file);
-//     let filename = path.file_name()?.to_str()?;
-//     println!("{}",filename);
-//     None
-// }
